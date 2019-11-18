4f73988b900ed76dd356ae871a1ad273a04d6ba3
Galen Brown <galen.brown.18@dartmouth.edu>
2019-11-18 14:05:16 -0500

    Removed extra s

--- FileHandler.py
+++ FileHandler.py
@@ -22,6 +22,9 @@ def getBriefData(defaults='PAD'):
   elif defaults == 'PADTemp':
     samplesTemp = ['Amoxicillin', 'Amoxicillin clavulonic acid', 'Ampicillin', 'Azithromycin', 'Benzyl Penicillin', 'Ceftriaxone', 'Chloroquine', 'Ciprofloxacin', 'Corn Starch', 'DI water', 'Doxycycline', 'Enalapril', 'Isoniazid', 'Losartan', 'Metformin', 'Paracetamol', 'Penicillin Procaine',  'Quinine', 'Tap water', 'Unknown']
     return {'tests':tests, 'samples':samplesTemp, 'categories':categories}
+  elif defaults == 'ChemoPAD':
+    chemoDrugs ['Doxorubicin', 'Cisplatin', 'Oxaliplatin', 'Methotrexate']
+    return {'tests':tests, 'samples':chemoDrugs, 'categories':categories}
 
 def getData(defaults='PAD'):
   tests = ImageUploading.getData('tests')
